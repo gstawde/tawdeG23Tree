5f75c358ad9144d3c54540b81651074c108ac7b2
gstawde <gargi.tawde01@gmail.com>
2020-09-28 01:26:03 -0700

    Initial Commit: 2-3 Tree Implementation with ability to perform an upwards split. (Cannot handle cascading splits.)

---
+++ src/Tree.java
@@ -0,0 +1,249 @@
+import java.util.TreeSet;
+
+interface TwoThreeTree {
+    public boolean insert(int x);
+    public int size(int x);
+}
+
+
+public class Tree implements TwoThreeTree {
+
+    Node root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public boolean insert(int x) {
+        if (root == null) {
+            root = new Node(x, null);
+            return true;
+        }
+        Node current = root;
+        // while we haven't reached an end point,
+        while(true) {
+            if (current.contains(x)) {
+                return false;
+            }
+            if (current.add(x)) {
+                break;
+            } else {
+                Node next = current.getSubTree(x);
+                current = next;
+            }
+        }
+        split(current);
+
+        return true;
+    }
+
+    public void split(Node current) {
+        if (!current.needsSplitting()) {
+            return;
+        }
+        if (current.getParent() == null) {
+            // make new nodes
+            Node left = current.splitLeft();
+            Node right = current.splitRight();
+            // set their parents to the root
+            left.setParent(current);
+            right.setParent(current);
+            // set left and right of new root
+            current.setLeft(left);
+            current.setRight(right);
+            return;
+        }
+        // make new nodes
+        Node parent = current.getParent();
+        Node left = current.splitLeft();
+        Node right = current.splitRight();
+        // add middle key to parent node
+        parent.addKey(current.getFirst());
+        // check if current is smallest or largest
+        if (parent.getFirst() == current.getFirst()) {
+            parent.setLeft(left);
+            parent.setMiddle(right);
+        } else {
+            parent.setMiddle(left);
+            parent.setRight(right);
+        }
+        split(parent);
+    }
+
+    public int size(int x) {
+        // find Node with a key of value x
+        Node n = search(x, root);
+        if (n == null) {
+            return 0;
+        }
+        return size(n);
+    }
+
+    public int size(Node n) {
+        if (n == null) {
+            return 0;
+        }
+        int size = n.getSize();
+        size += size(n.getLeft());
+        size += size(n.getMiddle());
+        size += size(n.getRight());
+        return size;
+    }
+
+    public Node search(int x, Node start) {
+        if (start == null) {
+            return null;
+        }
+        if (start.contains(x)) {
+            return start;
+        }
+        return search(x, start.getSubTree(x)); // continue searching with next appropriate subtree based on value of x
+    }
+
+    @Override
+    public String toString() {
+        return toString(root);
+    }
+
+    private String toString(Node n) {
+        String tree = "";
+        if (n == null) {
+            return tree;
+        }
+        tree += n.getKeys();
+        tree += "(" + toString(n.getLeft()) + "," + toString(n.getMiddle()) + "," + toString(n.getRight()) + ")";
+        return tree;
+    }
+
+
+    class Node {
+
+
+        TreeSet<Integer> keys = new TreeSet<>();
+        Node left = null;
+        Node middle = null;
+        Node right = null;
+        Node parent = null;
+
+        public Node(int key1, Node parent) {
+            keys.add(key1);
+            this.parent = parent;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getMiddle() {
+            return middle;
+        }
+
+        public void setMiddle(Node middle) {
+            this.middle = middle;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Integer getFirst() {
+            return keys.first();
+        }
+
+        public TreeSet<Integer> getKeys() {
+            return keys;
+        }
+
+        public boolean isLeaf() {
+            if (keys.size() == 1) {
+                // 2-node
+                return (left == null && right == null);
+            } else {
+                // 3-node
+                return (left == null && middle == null && right == null);
+            }
+        }
+
+        public boolean needsSplitting() {
+            return keys.size() > 2;
+        }
+
+        public Node splitLeft() {
+            Node left = new Node(keys.first(), parent);
+            keys.remove(keys.first());
+            return left;
+        }
+
+        public Node splitRight() {
+            Node right = new Node(keys.last(), parent);
+            keys.remove(keys.last());
+            return right;
+        }
+
+        public Node getSubTree(int key) {
+            if (keys.size() == 1) {
+                // 2-node
+                if (key < keys.first()) {
+                    return left;
+                } else {
+                    return right;
+                }
+            } else {
+                // 3 - node
+                if (key < keys.first()) {
+                    return left;
+                } else if (key < keys.last()) {
+                    return middle;
+                } else {
+                    return right;
+                }
+            }
+        }
+
+        public boolean contains(int x) {
+            if (keys.size() == 1) {
+                return x == keys.first();
+            }
+            return (x == keys.first() || x == keys.last());
+        }
+
+        public boolean add(int x) {
+            if (isLeaf()) {
+                keys.add(x);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public void addKey(int x) {
+            keys.add(x);
+        }
+
+        public int getSize() {
+            return keys.size();
+        }
+    }
+
+
+}
---
+++ src/TreeTests.java
@@ -0,0 +1,155 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+public class TreeTests {
+
+    // TESTING TREE CLASS
+
+    @Test
+    public void testInsertRoot() { // Also tests empty constructor for Tree class
+        Tree nums = new Tree();
+        nums.insert(5);
+        assertEquals(1, nums.size(5));
+    }
+
+    @Test
+    public void testFindingValueNotInTree() {
+        Tree nums = new Tree();
+        nums.insert(5);
+        assertEquals(1, nums.size(5));
+        assertEquals(0, nums.size(10));
+        assertEquals(0, nums.size(1));
+    }
+
+    @Test
+    public void testInsertingSameValueTwice() {
+        Tree nums = new Tree();
+        nums.insert(5);
+        nums.insert(5);
+    }
+
+    @Test
+    public void testFindingLeafToInsertAt() {
+        Tree nums = new Tree();
+        nums.insert(5);
+        nums.insert(10);
+        nums.insert(1);
+        nums.insert(11);
+    }
+
+    @Test
+    public void testAddBiggerNumberToNodeWithSpace() {
+        Tree nums = new Tree();
+        nums.insert(5);
+        nums.insert(10);
+        assertEquals(2, nums.size(5));
+        assertEquals(2, nums.size(10));
+        assertEquals(5, nums.getRoot().getFirst(), 0.000001);
+    }
+
+    @Test
+    public void testAddSmallerNumberToNodeWithSpace() {
+        Tree nums = new Tree();
+        nums.insert(10);
+        nums.insert(5);
+        assertEquals(2, nums.size(5));
+        assertEquals(2, nums.size(10));
+        assertEquals(5, nums.getRoot().getFirst(), 0.000001);
+    }
+
+    @Test
+    public void testMoveLeftUpToParent() {
+        Tree nums = new Tree();
+        nums.insert(10);
+        nums.insert(5);
+        nums.insert(1);
+        nums.insert(8);
+        nums.insert(11);
+    }
+
+    @Test
+    public void testSplitLeft() {
+        Tree nums = new Tree();
+        nums.insert(10);
+        nums.insert(5);
+        nums.insert(1);
+        assertEquals(3, nums.size(5));
+        assertEquals(1, nums.size(10));
+        assertEquals(1, nums.size(1));
+        assertEquals(5, nums.getRoot().getFirst(), 0.000001);
+    }
+
+    @Test
+    public void testSplitRight() {
+        Tree nums = new Tree();
+        nums.insert(1);
+        nums.insert(5);
+        nums.insert(10);
+        assertEquals(3, nums.size(5));
+        assertEquals(1, nums.size(10));
+        assertEquals(1, nums.size(1));
+        assertEquals(5, nums.getRoot().getFirst(), 0.000001);
+    }
+
+    @Test
+    public void testSplitMiddle() {
+        Tree nums = new Tree();
+        nums.insert(1);
+        nums.insert(10);
+        nums.insert(5);
+        assertEquals(3, nums.size(5));
+        assertEquals(1, nums.size(10));
+        assertEquals(1, nums.size(1));
+        assertEquals(5, nums.getRoot().getFirst(), 0.000001);
+    }
+
+
+    // TESTING NODE CLASS
+
+    @Test
+    public void testGetKeys() {
+        Tree nums = new Tree();
+        nums.insert(1);
+        nums.insert(10);
+        nums.insert(5);
+        nums.toString();
+    }
+
+    @Test
+    public void testGet3NodeSubtreeAddToLeft() {
+        Tree nums = new Tree();
+        nums.insert(1);
+        nums.insert(10);
+        nums.insert(5);
+        nums.insert(2);
+        nums.insert(3);
+        nums.insert(0);
+    }
+
+    @Test
+    public void testGet3NodeSubtreeAddToMiddle() {
+        Tree nums = new Tree();
+        nums.insert(1);
+        nums.insert(10);
+        nums.insert(5);
+        nums.insert(2);
+        nums.insert(3);
+        nums.insert(4);
+    }
+
+    @Test
+    public void testGet3NodeSubtreeAddToRight() {
+        Tree nums = new Tree();
+        nums.insert(1);
+        nums.insert(10);
+        nums.insert(5);
+        nums.insert(2);
+        nums.insert(3);
+        nums.insert(8);
+    }
+
+
+}
